2e6a5ee4ef59c759f5e656182d07340a211b9ff0
DamianMartinDominguez <64601914+DamianMartinDominguez@users.noreply.github.com>
2022-09-19 09:30:27 -0300

    se agrego el nombre del equipo

--- ejemplo_blink.c
+++ ejemplo_blink.c
@@ -6,8 +6,8 @@
 
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
-#define BLYNK_TEMPLATE_ID           "TMPLORztyrJf"
-#define BLYNK_DEVICE_NAME           "Quickstart Device"
+#define BLYNK_TEMPLATE_ID "TMPLORztyrJf"
+#define BLYNK_DEVICE_NAME "Reset Remoto"
 #define BLYNK_AUTH_TOKEN            "kJx0DF9pLtSYwmwh3nNojjmfv04nKMaC"
 
 
